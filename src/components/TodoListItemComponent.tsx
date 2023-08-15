e0cce76e1693cab9da84009031674adf7553c3ba
nara04040 <nara04040@naver.com>
2023-08-15 13:58:56 +0900

    todolist 생성, 수정 기능 개발

--- src/components/TodoListItemComponent.tsx
+++ src/components/TodoListItemComponent.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
 
-export default function TodoListItemComponent() {
+type TodoItem = {
+  todo: string;
+};
+
+interface TodoListProps {
+  todoLists: TodoItem[];
+}
+
+export default function TodoListItemComponent({ todoLists }: TodoListProps) {
+  const [amendTodoList, setamendTodoList] = useState<number | null>(null);
+  const [amendValue, setAmendValue] = useState("");
+
+  const handleEditClick = (index: number) => {
+    setAmendValue(todoLists[index].todo);
+    setamendTodoList(index);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmendValue(e.target.value);
+  };
+
   return (
     <div className="todo_list_item_wrapper">
-      <div className="todo_list_items">
-        <div className="todo_list_items_checkbox"></div>
-        <div className="todo_lisst_items_textbox">asdfasdfasdfasdfasdfasdfasdf</div>
-      </div>
-      <div className="todo_list_items">
-        <div className="todo_list_items_checkbox"></div>
-        <div className="todo_lisst_items_textbox">22</div>
-      </div>
+      {todoLists.map((todoItem, i) => (
+        <div className="todo_list_items" key={i}>
+          <div className="todo_list_items_checkbox"></div>
+
+          {amendTodoList === i ? <input value={amendValue} onChange={handleInputChange} autoFocus /> : <div className="todo_list_items_textbox">{todoItem.todo}</div>}
+
+          <div className="amend_list_items" onClick={() => handleEditClick(i)}>
+            <FontAwesomeIcon icon={faPen} />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/TodoListTemplateComponent.tsx
+++ src/components/TodoListTemplateComponent.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import TodoListTitleComponent from "./TodoListTitleComponent";
 import TodoListInputComponent from "./TodoListInputComponent";
 import TodoListItemComponent from "./TodoListItemComponent";
 import "../App.css";
 
 export default function TodoListTemplateComponent() {
+  const [todoLists, setTodoLists] = useState([{ todo: "list1" }, { todo: "list2" }, { todo: "list3" }]);
+
   return (
     <>
       <div className="todo_list_container">
         <TodoListTitleComponent />
-        <TodoListInputComponent />
-        <TodoListItemComponent />
+        <TodoListInputComponent setTodoLists={setTodoLists} />
+        <TodoListItemComponent todoLists={todoLists} />
       </div>
     </>
   );
